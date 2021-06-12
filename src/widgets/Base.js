f00867674603b1da636a8452929dcb12450d55b9
Philipp Schmiedel <philipp.schmiedel@gmail.com>
2021-06-12 18:06:37 +0200

    css fixes

--- src/widgets/Base.js
+++ src/widgets/Base.js
@@ -17,10 +17,20 @@ class Base {
      */
     this.el = el;
     /**
-     * Dimensions of the original element
+     * Calculated dimensions of the original element
      * @type {DOMRect}
      */
     this.dimensions = this.el.getBoundingClientRect();
+    /**
+     * Original node width
+     * @type {string|number}
+     */
+    this.width = this.el.getAttribute('width');
+    /**
+     * Original node height
+     * @type {string|number}
+     */
+    this.height = this.el.getAttribute('height');
     /**
      * Usercentrics Service ID defined via data-uc-id on the widget
      * @type {string}
@@ -85,6 +95,10 @@ class Base {
 
     container.innerHTML = this.getEmbedding();
     container.setAttribute('class', 'uc-widget-container');
+
+    const width = this.width ? (isNaN(this.width) ? this.width : `${this.width}px`) : `${this.dimensions.width}px`;
+    const height = this.height ? (isNaN(this.height) ? this.height : `${this.height}px`) : `${this.dimensions.height}px`;
+    container.setAttribute('style', `width: ${width}; height: ${height};`);
 
     this.el.replaceWith(container);
 
